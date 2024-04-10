b739285d24221bb02dc31a0b2e3c668b8b5fd164
yiseoffline <lsy0476@hufs.ac.kr>
2024-04-10 17:46:23 +0900

    feat : create router & counter.jsx

---
+++ week2/todolist/src/counter/Counter.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+const Counter = () => {
+  const [counter, setCounter] = useState(0);
+
+  const plus = () => {
+    setCounter((counter) => counter + 1);
+  };
+
+  const minus = () => {
+    setCounter((counter) => counter - 1);
+  };
+
+  return (
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <h1>{counter}</h1>
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        <button style={{ marginRight: "10px" }} onClick={plus}>
+          +1
+        </button>
+        <button onClick={minus}>-1</button>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
